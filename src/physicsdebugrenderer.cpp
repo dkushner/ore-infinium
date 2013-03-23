3f473940101b82e85ef15716f1351d3f1b63f46b
Shaun Reich <sreich@kde.org>
2013-03-23 14:22:20 -0400

    begin attempting to use programmable pipeline for the box2d debug renderer

--- src/physicsdebugrenderer.cpp
+++ src/physicsdebugrenderer.cpp
@@ -17,19 +17,126 @@
 
 #include "physicsdebugrenderer.h"
 #include "debug.h"
+#include "world.h"
+
+PhysicsDebugRenderer::PhysicsDebugRenderer()
+{
+    m_shader = new Shader("sprite.vert", "sprite.frag");
+
+    Debug::checkGLError();
+    initGL();
+    Debug::checkGLError();
+}
+
+PhysicsDebugRenderer::~PhysicsDebugRenderer()
+{
+    glDeleteBuffers(1, &m_vbo);
+    glDeleteBuffers(1, &m_ebo);
+
+    glDeleteVertexArrays(1, &m_vao);
+}
+
+void PhysicsDebugRenderer::initGL()
+{
+    ///////////CHARACTERS////////////////
+    glGenVertexArrays(1, &m_vao);
+    glBindVertexArray(m_vao);
+
+    glGenBuffers(1, &m_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        m_maxSpriteCount * 4 * sizeof(Vertex),
+                 NULL,
+                 GL_DYNAMIC_DRAW);
+
+    Debug::checkGLError();
+
+    /*
+    std::vector<u32> indicesv;
+
+    // prepare and upload indices as a one time deal
+    const u32 indices[] = { 0, 1, 2, 0, 2, 3 }; // pattern for a triangle array
+    // for each possible sprite, add the 6 index pattern
+    for (size_t j = 0; j < m_maxSpriteCount; j++) {
+        for (size_t i = 0; i < sizeof(indices) / sizeof(*indices); i++) {
+            indicesv.push_back(4 * j + indices[i]);
+        }
+    }
+
+    glGenBuffers(1, &m_ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ebo);
+    glBufferData(
+        GL_ELEMENT_ARRAY_BUFFER,
+        indicesv.size()*sizeof(u32),
+                 indicesv.data(),
+                 GL_STATIC_DRAW);
+                 */
+
+    Debug::checkGLError();
+
+    size_t buffer_offset = 0;
+
+    GLint pos_attrib = glGetAttribLocation(m_shader->shaderProgram(), "position");
+    glEnableVertexAttribArray(pos_attrib);
+    glVertexAttribPointer(
+        pos_attrib,
+        2,
+        GL_FLOAT,
+        GL_FALSE,
+        sizeof(Vertex),
+                          (const GLvoid*)buffer_offset);
+    buffer_offset += sizeof(f32) * 2;
+
+    GLint color_attrib = glGetAttribLocation(m_shader->shaderProgram(), "color");
+
+    Debug::checkGLError();
+
+    glEnableVertexAttribArray(color_attrib);
+    glVertexAttribPointer(
+        color_attrib,
+        4,
+        GL_UNSIGNED_BYTE,
+        GL_TRUE,
+        sizeof(Vertex),
+                          (const GLvoid*)buffer_offset);
+    buffer_offset += sizeof(u32);
+
+    Debug::checkGLError();
+
+    GLint texcoord_attrib = glGetAttribLocation(m_shader->shaderProgram(), "texcoord");
+    glEnableVertexAttribArray(texcoord_attrib);
+    glVertexAttribPointer(
+        texcoord_attrib,
+        2,
+        GL_FLOAT,
+        GL_FALSE,
+        sizeof(Vertex),
+                          (const GLvoid*)buffer_offset);
+
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    Debug::checkGLError();
+
+}
 
 void PhysicsDebugRenderer::DrawPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color)
 {
-    glEnableClientState(GL_VERTEX_ARRAY);
+    /*
     glColor4f(color.r, color.g, color.b,1);
     glVertexPointer(2, GL_FLOAT, 0, vertices);
     glDrawArrays(GL_LINE_LOOP, 0, vertexCount);
-    glDisableClientState(GL_VERTEX_ARRAY);
+    */
 }
 
 void PhysicsDebugRenderer::DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color)
 {
-    glEnableClientState(GL_VERTEX_ARRAY);
+//glTranslatef(100, 100, 0);
+
+/*    glViewport(500, 0, 1500, 900);
+//    vertices[0].x = 2;
     glVertexPointer(2, GL_FLOAT, 0, vertices);
 
     glColor4f(color.r, color.g, color.b,0.5f);
@@ -37,12 +144,37 @@ void PhysicsDebugRenderer::DrawSolidPolygon(const b2Vec2* vertices, int32 vertex
 
     glColor4f(color.r, color.g, color.b,1);
     glDrawArrays(GL_LINE_LOOP, 0, vertexCount);
-    glDisableClientState(GL_VERTEX_ARRAY);
+    glViewport(0, 0, 1600, 900);
+    */
+//set up vertex array
+/*
+GLfloat glverts[16]; //allow for polygons up to 8 vertices
+glVertexPointer(2, GL_FLOAT, 0, glverts); //tell OpenGL where to find vertices
+glEnableClientState(GL_VERTEX_ARRAY); //use vertices in subsequent calls to glDrawArrays
+
+//fill in vertex positions as directed by Box2D
+for (int i = 0; i < vertexCount; i++) {
+    glverts[i*2]   = vertices[i].x;
+    glverts[i*2+1] = vertices[i].y;
+}
+
+//draw solid area
+glColor4f( color.r, color.g, color.b, 1);
+glDrawArrays(GL_TRIANGLE_FAN, 0, vertexCount);
+
+//draw lines
+glLineWidth(3); //fat lines
+glColor4f( 1, 0, 1, 1 ); //purple
+glDrawArrays(GL_LINE_LOOP, 0, vertexCount);
+
+glPopMatrix();
+*/
+
 }
 
 void PhysicsDebugRenderer::DrawCircle(const b2Vec2& center, float32 radius, const b2Color& color)
 {
-    glEnableClientState(GL_VERTEX_ARRAY);
+    /*
     const float32 k_segments = 16.0f;
     int vertexCount=16;
     const float32 k_increment = 2.0f * b2_pi / k_segments;
@@ -61,12 +193,12 @@ void PhysicsDebugRenderer::DrawCircle(const b2Vec2& center, float32 radius, cons
     glVertexPointer(2, GL_FLOAT, 0, glVertices);
 
     glDrawArrays(GL_TRIANGLE_FAN, 0, vertexCount);
-    glDisableClientState(GL_VERTEX_ARRAY);
+    */
 }
 
 void PhysicsDebugRenderer::DrawSolidCircle(const b2Vec2& center, float32 radius, const b2Vec2& axis, const b2Color& color)
 {
-    glEnableClientState(GL_VERTEX_ARRAY);
+    /*
     const float32 k_segments = 16.0f;
     int vertexCount=16;
     const float32 k_increment = 2.0f * b2_pi / k_segments;
@@ -87,22 +219,22 @@ void PhysicsDebugRenderer::DrawSolidCircle(const b2Vec2& center, float32 radius,
     glColor4f(color.r, color.g, color.b,1);
     glDrawArrays(GL_LINE_LOOP, 0, vertexCount);
 
-    glDisableClientState(GL_VERTEX_ARRAY);
 
     // Draw the axis line
     DrawSegment(center,center+radius*axis,color);
+    */
 }
 
 void PhysicsDebugRenderer::DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color)
 {
-    glEnableClientState(GL_VERTEX_ARRAY);
+    /*
     glColor4f(color.r, color.g, color.b,1);
     GLfloat                         glVertices[] = {
         p1.x,p1.y,p2.x,p2.y
     };
     glVertexPointer(2, GL_FLOAT, 0, glVertices);
     glDrawArrays(GL_LINES, 0, 2);
-    glDisableClientState(GL_VERTEX_ARRAY);
+    */
 }
 
 void PhysicsDebugRenderer::DrawTransform(const b2Transform& xf)
@@ -121,7 +253,7 @@ void PhysicsDebugRenderer::DrawTransform(const b2Transform& xf)
 
 void PhysicsDebugRenderer::DrawPoint(const b2Vec2& p, float32 size, const b2Color& color)
 {
-    glEnableClientState(GL_VERTEX_ARRAY);
+    /*
     glColor4f(color.r, color.g, color.b,1);
     glPointSize(size);
     GLfloat                         glVertices[] = {
@@ -130,7 +262,7 @@ void PhysicsDebugRenderer::DrawPoint(const b2Vec2& p, float32 size, const b2Colo
     glVertexPointer(2, GL_FLOAT, 0, glVertices);
     glDrawArrays(GL_POINTS, 0, 1);
     glPointSize(1.0f);
-    glDisableClientState(GL_VERTEX_ARRAY);
+    */
 }
 
 void PhysicsDebugRenderer::DrawString(int x, int y, const char *string, ...)
@@ -141,8 +273,7 @@ void PhysicsDebugRenderer::DrawString(int x, int y, const char *string, ...)
 
 void PhysicsDebugRenderer::DrawAABB(b2AABB* aabb, const b2Color& c)
 {
-    glEnableClientState(GL_VERTEX_ARRAY);
-
+    /*
     glColor4f(c.r, c.g, c.b,1);
 
     GLfloat                         glVertices[] = {
@@ -153,6 +284,86 @@ void PhysicsDebugRenderer::DrawAABB(b2AABB* aabb, const b2Color& c)
     };
     glVertexPointer(2, GL_FLOAT, 0, glVertices);
     glDrawArrays(GL_LINE_LOOP, 0, 8);
-    glDisableClientState(GL_VERTEX_ARRAY);
+    */
+}
 
+
+void PhysicsDebugRenderer::render()
+{
+
+    for (Box2DQuad box2DQuad: m_solidPolygons) {
+    // vertices that will be uploaded.
+    std::vector<Vertex> vertices;
+
+    for (int i = 0; i < box2DQuad.vertexCount; ++i) {
+        Vertex vert;
+        vert.x
+       vertices.push_back(vert);
+    }
+
+    glGenBuffers(1, &m_ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ebo);
+    glBufferData(
+        GL_ELEMENT_ARRAY_BUFFER,
+        vertices.size()*sizeof(Vertex),
+                 vertices.data(),
+                 GL_STATIC_DRAW);
+
+    glm::vec2 spritePosition = box2DQuad.
+
+    glm::vec2 spriteSize = sprite->size();
+
+    Debug::checkGLError();
+    /*
+    // copy color to the buffer
+    for (size_t i = 0; i < sizeof(vertices) / sizeof(*vertices); i++) {
+        //        *colorp = color.bgra;
+        uint8_t red = 255;
+        uint8_t green = 255;
+        uint8_t blue = 255;
+        uint8_t alpha = 255;
+        int32_t color = red | (green << 8) | (blue << 16) | (alpha << 24);
+        vertices[i].color = color;
+    }
+    */
+
+    // finally upload everything to the actual vbo
+    glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+    glBufferSubData(
+        GL_ARRAY_BUFFER,
+        sizeof(vertices) * index,
+                    sizeof(vertices),
+                    vertices);
+
+    ++index;
+}
+
+    ////////////////////////////////FINALLY RENDER IT ALL //////////////////////////////////////////
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ebo);
+    glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+    glBindVertexArray(m_vao);
+
+    Debug::checkGLError();
+
+    m_shader->bindProgram();
+
+    Debug::checkGLError();
+
+    glDrawElements(
+        GL_TRIANGLES,
+        6 * (m_solidPolygons.size()), // 6 indices per 2 triangles
+                GL_UNSIGNED_INT,
+                (const GLvoid*)0);
+
+    m_shader->unbindProgram();
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    glDisable(GL_BLEND);
+
+    Debug::checkGLError();
 }
--- src/physicsdebugrenderer.h
+++ src/physicsdebugrenderer.h
@@ -20,22 +20,78 @@
 
 #include <GL/glew.h>
 #include <Box2D/Box2D.h>
+#include <glm/core/type.hpp>
+
+#include "src/camera.h"
+#include "src/shader.h"
 
 class PhysicsDebugRenderer : public b2Draw {
 public:
+    PhysicsDebugRenderer();
+    virtual ~PhysicsDebugRenderer();
+
     virtual void DrawPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color);
+    virtual void DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color);
 
-    void DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color);
-    void DrawCircle(const b2Vec2& center, float32 radius, const b2Color& color);
-    void DrawSolidCircle(const b2Vec2& center, float32 radius, const b2Vec2& axis, const b2Color& color);
-    void DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color);
-    void DrawTransform(const b2Transform& xf);
+    virtual void DrawCircle(const b2Vec2& center, float32 radius, const b2Color& color);
+    virtual void DrawSolidCircle(const b2Vec2& center, float32 radius, const b2Vec2& axis, const b2Color& color);
 
-    void DrawPoint(const b2Vec2& p, float32 size, const b2Color& color);
+    virtual void DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color);
+    virtual void DrawTransform(const b2Transform& xf);
 
-    void DrawString(int x, int y, const char* string, ...);
+    virtual void DrawPoint(const b2Vec2& p, float32 size, const b2Color& color);
 
-    void DrawAABB(b2AABB* aabb, const b2Color& color);
+    virtual void DrawString(int x, int y, const char* string, ...);
+
+    virtual void DrawAABB(b2AABB* aabb, const b2Color& color);
+
+    void render();
+
+private:
+    void initGL();
+
+private:
+    typedef uint32_t u32;
+    typedef float f32;
+
+    /* Each vertex is:
+     * two floats for the 2d coordinate
+     * four u8s for the color
+     * two f32s for the texcoords
+     * the vbo contains data of the aforementioned elements interleaved.
+     * Each sprite has four vertices.
+     * */
+    struct Vertex {
+        float x, y;
+        unsigned int color; // packed with 4 u8s (unsigned chars) for color
+
+        float u = 0.0f;
+        float v = 0.0f;
+    };
+
+    struct Box2DQuad {
+        b2Vec2* vec;
+        int32_t vertexCount;
+        b2Color color;
+    };
+
+    std::vector<Box2DQuad> m_solidPolygons;
+
+    GLuint m_vao; // vertex array object
+    GLuint m_vbo; // vertex buffer object
+    GLuint m_ebo; // element buffer object
+
+    GLuint m_vaoEntities; // vertex array object
+    GLuint m_vboEntities; // vertex buffer object
+    GLuint m_eboEntities; // element buffer object
+
+    glm::mat4 m_modelMatrix;
+    glm::mat4 m_projectionMatrix;
+
+    int m_maxSpriteCount = 2200;
+
+    Camera* m_camera = nullptr;
+    Shader* m_shader = nullptr;
 };
 
 #endif
