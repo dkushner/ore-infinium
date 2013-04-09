4852fd8adb578966615022e2dc506120a7530d3d
Shaun Reich <sreich@kde.org>
2013-04-09 19:53:47 -0400

    add another vert to this..i don't think this can scale well
    i'm thinking that using 2 circles..or 3 is a better approach for the
    character bodyies.

--- src/player.cpp
+++ src/player.cpp
@@ -63,7 +63,7 @@ void Player::createPhysicsBody(World* world, const glm::vec2& position)
     b2PolygonShape dynamicBox;
 //    dynamicBox.SetAsBox(size.x * 0.5f, size.y * 0.5f);
 
-    int vertCount = 4;
+    int vertCount = 5;
     b2Vec2 vertices[vertCount];
 //
 //    vertices[0].Set(2.1f, 1.1f); // top left
@@ -84,7 +84,8 @@ void Player::createPhysicsBody(World* world, const glm::vec2& position)
     vertices[0].Set(0, -1);
     vertices[1].Set(1, 1);
     vertices[2].Set(0, 3);
-    vertices[3].Set(-1, 1);
+    vertices[3].Set(-1.0f, 2.9);
+    vertices[4].Set(-1, 1);
 
 //    vertices[4].Set(6.1f, 5.5f); // center right
 //    vertices[5].Set(7.1f, 6.9f); // bottom left
